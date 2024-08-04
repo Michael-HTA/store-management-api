b17b6267f2ccee9ee10eccfa8f4bab4fa1a6fc76
Hein Thu Aung <hnthu.aung@gmail.com>
2024-08-04 19:21:22 +0630

    ec

--- app/Interfaces/ProductInterface.php
+++ app/Interfaces/ProductInterface.php
@@ -6,7 +6,7 @@ use App\Http\Requests\ProductStoreRequest;
 interface ProductInterface{
 
     public function storeProduct(array $data);
-    public function getProduct();
+    public function getProduct(int $perPage);
     public function findProduct(int $id);
     public function updateProduct(int $id, array $data);
     public function deleteProduct(int $id);
--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -1,49 +1,61 @@
 <?php
+
 namespace App\Services;
 
-use App\Http\Requests\ProductStoreRequest;
 use App\Interfaces\ProductInterface;
 use App\Models\Product;
 
-class ProudctService implements ProductInterface{
+class ProductService implements ProductInterface
+{
 
     public function __construct(protected Product $product)
     {
+    }
+
+    protected function getProductById($id){
+
+        return $this->product->where('id',$id)->first();
 
     }
 
     public function storeProduct(array $data)
-    {
+    {   
+        return $this->product->create($data);
 
     }
 
-    public function getProduct(){
-
+    public function getProduct(int $perPage)
+    {
+        return $this->product->with(['manufacturer', 'supplier', 'modelForm', 'category'])->paginate($perPage);
     }
 
     public function getOutOfStock(int $stock = 5, int $limit = null)
     {
-        if($stock < 0) return false;
+        $query = $this->product->where('stock', '<', $stock);
 
-        $query = $this->product::where('stock','<',$stock);
+        if ($limit !== null) $query->limit($limit);
 
-        if(is_null($limit)) return $query->get();
-
-        return $query->limit($limit)->get();
+        return $query->get();
     }
 
     public function findProduct(int $id)
     {
-
+        return $this->product->with(['manufacturer', 'supplier', 'modelForm', 'category'])->where('id', $id)->first();
     }
 
     public function updateProduct(int $id, array $data)
     {
+        $product = $this->getProductById($id);
+
+        return $product ? $product->update($data) : null;
 
     }
 
     public function deleteProduct(int $id)
     {
+        $product = $this->getProductById($id);
+
+        return $product ? false : $product->delete();
 
     }
 }
